48f6c3f9b0e5077421720df78588bb35b6a77035
pilifalchi <falchipilar@gmail.com>
2023-03-17 00:41:39 -0300

    primeras 8 clases de rust

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Por favor indica tu pais: ");
+
+    let mut pais: String = String::new();
+    std::io::stdin().read_line(&mut pais).unwrap();
+
+    println!("Por favor introduce tu edad");
+    let mut edad: String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+
+    let edad_int : u8 = edad.trim().parse().unwrap();
+
+    println!("Hola, bienvenido usuario de {} de {} años de edad", pais, edad_int);
+}
+
+
